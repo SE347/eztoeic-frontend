f58961bdb656285cf4d21505b7c9b6359f5d30c6
Nguyen Tuan Khoi <nothingisforever020302@gmail.com>
2023-12-11 23:05:56 +0700

    chore: change method push

--- components/PartSolutionItem.tsx
+++ components/PartSolutionItem.tsx
@@ -1,8 +1,7 @@
 import { PartInfo } from "@/app/tests/[id]/page";
 import { UnstyledButton, Text, Modal } from "@mantine/core";
-import { useDisclosure } from "@mantine/hooks";
 import React from "react";
-import SolutionItem from "./SolutionItem";
+import { useRouter } from "next/navigation";
 
 function PartSolutionItem({
   partInfo,
@@ -11,7 +10,8 @@ function PartSolutionItem({
   partInfo: PartInfo | null;
   testId: string;
 }) {
-  const [opened, { open, close }] = useDisclosure(false);
+  // const [opened, { open, close }] = useDisclosure(false);
+  const router = useRouter();
   return (
     <div>
       {partInfo !== null ? (
@@ -25,7 +25,7 @@ function PartSolutionItem({
           <Text>•{partInfo!.name}:</Text>
           <UnstyledButton
             style={{ color: "var(--mantine-color-blue-filled)", marginLeft: 6 }}
-            onClick={open}
+            onClick={()=>{router.push(`/tests/1/solution/?part=${partInfo.value}`);}}
           >
             Answer
           </UnstyledButton>
@@ -33,12 +33,15 @@ function PartSolutionItem({
       ) : (
         <UnstyledButton
           style={{ color: "var(--mantine-color-blue-filled)" }}
-          onClick={open}
+          onClick={()=>{
+            const temp = ["1", "2", "3", "4", "5", "6", "7"].map((e) => `part=${e}`);
+      const query = temp.join("&");
+            router.push(`/tests/1/solution/?${query}`);}}
         >
           See test solution
         </UnstyledButton>
       )}
-      <Modal
+      {/* <Modal
         opened={opened}
         onClose={close}
         title={partInfo?.name ?? "Solution"}
@@ -46,7 +49,7 @@ function PartSolutionItem({
         styles={{ title: { fontWeight: "bold" } }}
       >
         <SolutionItem partId={partInfo?.value ?? null} testId={testId} />
-      </Modal>
+      </Modal> */}
     </div>
   );
 }
